009307161113854b885e4b1031a83f44854ac46a
Pheeleex <ohemufelix@gmail.com>
2024-05-22 05:04:59 +0100

    3d model

--- app/components/Canvas/ComputerCanvas.tsx
+++ app/components/Canvas/ComputerCanvas.tsx
@@ -1,12 +1,14 @@
-
+'use client'
 import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from '../Loader';
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Canvas } from "@react-three/fiber";
 
+interface props{
+  isMobile: boolean
+}
 
-
-const Computer = () => {
+const Computer = ({isMobile}: props) => {
   const { scene } = useGLTF('./desktop/scene.gltf'); // Destructure scene directly
 
   // Log to verify if the scene is loaded
@@ -26,7 +28,7 @@ const Computer = () => {
       <pointLight intensity={7} />
       <primitive
         object={scene}
-        scale={[4.5,2,2]}
+        scale={isMobile ?[3,1.5,2]: [4.5,2,2]}
         position={ [-0.8, -4.8, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
@@ -35,7 +37,21 @@ const Computer = () => {
 };
 
 const ComputersCanvas = () => {
-  
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event: MediaQueryListEvent) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
   return (
     <Canvas
       frameloop='demand'
@@ -51,7 +67,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Computer />
+        <Computer isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
