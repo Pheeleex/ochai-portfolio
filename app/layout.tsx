7c6fc1ecc2ed3e31ea40ca226d68f9babd126b4c
Pheeleex <ohemufelix@gmail.com>
2024-11-25 12:03:43 +0100

    analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import './globals.css'
+import { Analytics } from '@vercel/analytics/next';
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,7 +20,10 @@ export default function RootLayout({
 ) {
   return (
     <html lang="en">
-      <body className='bg-black-100'>{children}</body>
+      <body className='bg-black-100'>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
